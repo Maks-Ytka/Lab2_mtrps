ffbd2fc0d40d39df4aa207b3878104c7db3f694a
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2025-06-19 14:42:24 +0300

    Add insert, delete, deleteAll, findFirst and findLast to ArrayList

--- array_list.py
+++ array_list.py
@@ -12,3 +12,28 @@ class ArrayList:
         if index < 0 or index >= len(self.data):
             raise IndexError("Index out of bounds")
         return self.data[index]
+
+    def insert(self, element, index):
+        if index < 0 or index > len(self.data):
+            raise IndexError("Index out of bounds")
+        self.data.insert(index, element)
+
+    def delete(self, index):
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of bounds")
+        return self.data.pop(index)
+
+    def deleteAll(self, element):
+        self.data = [x for x in self.data if x != element]
+
+    def findFirst(self, element):
+        for i, val in enumerate(self.data):
+            if val == element:
+                return i
+        return -1
+
+    def findLast(self, element):
+        for i in range(len(self.data) - 1, -1, -1):
+            if self.data[i] == element:
+                return i
+        return -1
--- test_array_list.py
+++ test_array_list.py
@@ -18,3 +18,39 @@ def test_get_invalid_index():
     lst.append('z')
     with pytest.raises(IndexError):
         lst.get(1)
+
+def test_insert_and_delete():
+    lst = ArrayList()
+    lst.append('a')
+    lst.append('c')
+    lst.insert('b', 1)
+    assert lst.get(1) == 'b'
+    removed = lst.delete(1)
+    assert removed == 'b'
+    assert lst.length() == 2
+
+def test_insert_invalid_index():
+    lst = ArrayList()
+    with pytest.raises(IndexError):
+        lst.insert('x', 5)
+
+def test_delete_invalid_index():
+    lst = ArrayList()
+    with pytest.raises(IndexError):
+        lst.delete(0)
+
+def test_deleteAll():
+    lst = ArrayList()
+    for c in ['a', 'b', 'a', 'c']:
+        lst.append(c)
+    lst.deleteAll('a')
+    assert lst.length() == 2
+    assert lst.get(0) == 'b'
+
+def test_findFirst_and_findLast():
+    lst = ArrayList()
+    for c in ['x', 'y', 'x', 'z']:
+        lst.append(c)
+    assert lst.findFirst('x') == 0
+    assert lst.findLast('x') == 2
+    assert lst.findFirst('q') == -1
